46c6a235591d214fbf5be66c4e5eb8403973b92c
Aleix <afargas92@gmail.com>
2016-04-06 12:32:10 +0200

    added project files

---
+++ HealthManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HealthManager
+//
+//  Created by Aleix on 23/3/16.
+//  Copyright © 2016 Aleix Fargas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HealthManager/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  HealthManager
+//
+//  Created by Aleix on 23/3/16.
+//  Copyright © 2016 Aleix Fargas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    @IBOutlet var LoginView: NSView!
+    @IBOutlet weak var UserName: NSTextFieldCell!
+    @IBOutlet weak var UserPwd: NSSecureTextFieldCell!
+    @IBOutlet weak var UserType: NSComboBoxCell!
+    @IBOutlet weak var SignIn: NSButtonCell!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //Background color Layout
+        self.view.wantsLayer = true
+    }
+
+    override func viewWillAppear() {
+        //set the Background layout color
+        LoginView.layer?.backgroundColor = NSColor.whiteColor().CGColor
+        //LoginView.layer?.setNeedsDisplay()
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    @IBAction func onClickSignIn(sender: NSButtonCell) {
+        LoginView.removeFromSuperview()
+    }
+}
+
